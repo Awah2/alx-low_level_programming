f066d78bca0c5885e4af871780430cb1b40a3418
Awah2 <awahnji500@gmail.com>
2022-10-16 12:38:23 -0700

    .

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,8 +4,12 @@
  * Description: main - prints out a string to stdout.
  * Return: 0 if success
  */
-int main(void) 
+
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+		return (0);
+
 }
