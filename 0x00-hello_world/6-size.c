4cd8e55c93a9040ad719ea9adf1c66019b0801b6
Awah2 <awahnji500@gmail.com>
2022-10-16 14:02:18 -0700

    .

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,7 @@ int main(void)
 	long long int d;
 	float e;
 
-	printf("size of a char: %ld byte(s)\n", sizeof(c));
+	printf("size of a char: %ld byte(s)\n", sizeof(a));
 	printf("size of an int: %ld byte(s)\n", sizeof(b));
 	printf("size of a long int: %ld byte(s)\n", sizeof(c));
 	printf("size of a long long int: %ld byte(s)\n", sizeof(d));
